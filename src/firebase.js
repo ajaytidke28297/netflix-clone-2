96e472e5faad499e2fd9f61155ec407eb3c34acd
Ajay Tidke <ajaytidke28297@gmail.com>
2023-01-12 19:50:25 +0530

    Initial setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBvfyMlD4wjGjgoG4Ge0BwmI2NhixT61VM",
+  authDomain: "netflix-clone-63f75.firebaseapp.com",
+  projectId: "netflix-clone-63f75",
+  storageBucket: "netflix-clone-63f75.appspot.com",
+  messagingSenderId: "208676079185",
+  appId: "1:208676079185:web:bbe8b35982603d6d559a70",
+};
